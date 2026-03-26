b18d85ac991496d7cfd0a07fcdfa54a9bbd8eb1b
Facundo Silva <facusilv03@gmail.com>
2026-03-26 19:17:53 -0300

    service de java implementado

--- src/main/java/unlp/info/bd2/model/Service.java
+++ src/main/java/unlp/info/bd2/model/Service.java
@@ -1,14 +1,20 @@
 package unlp.info.bd2.model;
 
+import jakarta.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
 public class Service {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, length = 100, unique = true)
     private String name;
 
+    @Column(nullable = false)
     private float price;
 
     private String description;
